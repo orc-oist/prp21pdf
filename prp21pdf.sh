1b67567498229d4b39a097b90316cda4d14c6d01
orc-oist <ej.juarezperez@oist.jp>
2017-11-18 04:58:42 +0900

    first version of the prp21script

---
+++ prp21pdf.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#run the script inside the local github repository folder*
+cd ~/Desktop/prp21pdf
+
+#collect the html pages
+wget --recursive -l1 --no-parent -I policy-library/ --adjust-extension html  https://www.oist.jp/policy-library
+
+#cding into the html folder
+cd www.oist.jp/policy-library/
+
+#regexp collecting all links pointing to the pdfs and storing them as list in pdflist_new.txt
+grep -Phor '(?<=href=")[^"]*' *.html | grep pdf | grep -i 'public/ch'| sort > pdfslist_new.txt
+
+# collect the pdf files
+wget -i pdfslist_new.txt
+
+
+# preprocessing preparacion del listado de ficheros pdfs para pdfunite
+
+# replace blank spaces in the filenames by big dash 
+for f in *\ *; do mv "$f" "${f// /_}"; done
+
+# sorted list of files to file list
+ls -lht | grep -oE '([Cc][Hh].+\.pdf)' | sort  > pdfsnamefilelist.txt
+
+# merging sorted pdfs
+pdfunite $(cat pdfsnamefilelist.txt) fullprp.pdf
+
+
+# take snapshot apart if wednesday
+cd ~/Desktop/prp21pdf
+
+
+DAYOFWEEK=$(date +"%u")
+
+if [ "${DAYOFWEEK}" -eq 3 ];  
+	then   cp -r www.oist.jp $(date +%F-%T)_www.oist.jp;
+fi
