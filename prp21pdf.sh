4dbee6bb4d93c5c367d43fc6c49107327477d71e
orc-oist <ej.juarezperez@oist.jp>
2017-11-18 16:26:31 +0900

    cover for the pdf

--- prp21pdf.sh
+++ prp21pdf.sh
@@ -4,7 +4,7 @@
 cd ~/Desktop/prp21pdf
 
 #collect the html pages
-wget --recursive -l1 --no-parent -I policy-library/ --adjust-extension html  https://www.oist.jp/policy-library
+wget -N --recursive -l1 --no-parent -I policy-library/ --adjust-extension html  https://www.oist.jp/policy-library
 
 #cding into the html folder
 cd www.oist.jp/policy-library/
@@ -16,13 +16,20 @@ grep -Phor '(?<=href=")[^"]*' *.html | grep pdf | grep -i 'public/ch'| sort > pd
 wget -N -i pdfslist_new.txt
 
 
-# preprocessing preparacion del listado de ficheros pdfs para pdfunite
+# preprocessing list of pdf files before pdfunite
 
 # replace blank spaces in the filenames by big dash 
 for f in *\ *; do mv "$f" "${f// /_}"; done
 
 # sorted list of files to file list
 ls -lht | grep -oE '([Cc][Hh].+\.pdf)' | sort  > pdfsnamefilelist.txt
+
+# adding cover and version number
+date_time=$(date "+%Y-%m-%d %H:%M:%S")
+convert cover.png -pointsize 30 -fill white -annotate +150+400 "version: $date_time" cover.pdf
+
+#prepending the cover in the pdf files list
+echo -en "cover.pdf\n$(cat pdfsnamefilelist.txt)"  > pdfsnamefilelist.txt
 
 # merging sorted pdfs
 pdfunite $(cat pdfsnamefilelist.txt) fullprp.pdf
